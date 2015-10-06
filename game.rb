12ab5a822183ddd723eb402b06ecf40bce632fab
Mikel Aizpurua <mikel@aizpurua.me>
2015-10-06 15:53:13 -0400

    game simulator

---
+++ game.rb
@@ -0,0 +1,471 @@
+def pyramid
+  puts "
+
+                                          _____
+                                          _|[]_|_
+                                        _/_/=|_\_\_
+                                      _/_ /==| _\ _\_
+                                    _/__ /===|_ _\ __\_
+                                  _/_ _ /====| ___\  __\_
+                                _/ __ _/=====|_ ___\ ___ \_
+                              _/ ___ _/======| ____ \_  __ \_
+  "
+end
+
+
+
+def spacing
+  puts "
+
+                                *****************************
+
+  "
+end
+
+def game_title
+
+  puts '
+
+    ████████╗██╗███╗   ███╗███████╗    ████████╗██████╗  █████╗ ██╗   ██╗███████╗██╗     ███████╗██████╗
+    ╚══██╔══╝██║████╗ ████║██╔════╝    ╚══██╔══╝██╔══██╗██╔══██╗██║   ██║██╔════╝██║     ██╔════╝██╔══██╗
+       ██║   ██║██╔████╔██║█████╗         ██║   ██████╔╝███████║██║   ██║█████╗  ██║     █████╗  ██████╔╝
+       ██║   ██║██║╚██╔╝██║██╔══╝         ██║   ██╔══██╗██╔══██║╚██╗ ██╔╝██╔══╝  ██║     ██╔══╝  ██╔══██╗
+       ██║   ██║██║ ╚═╝ ██║███████╗       ██║   ██║  ██║██║  ██║ ╚████╔╝ ███████╗███████╗███████╗██║  ██║
+       ╚═╝   ╚═╝╚═╝     ╚═╝╚══════╝       ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝
+
+  '
+end
+
+def boom_title
+  puts '
+
+            ██████╗  █████╗ ███╗   ██╗ ██████╗     ██████╗  ██████╗  ██████╗ ███╗   ███╗
+            ██╔══██╗██╔══██╗████╗  ██║██╔════╝     ██╔══██╗██╔═══██╗██╔═══██╗████╗ ████║
+            ██████╔╝███████║██╔██╗ ██║██║  ███╗    ██████╔╝██║   ██║██║   ██║██╔████╔██║
+            ██╔══██╗██╔══██║██║╚██╗██║██║   ██║    ██╔══██╗██║   ██║██║   ██║██║╚██╔╝██║
+            ██████╔╝██║  ██║██║ ╚████║╚██████╔╝    ██████╔╝╚██████╔╝╚██████╔╝██║ ╚═╝ ██║
+            ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝     ╚═════╝  ╚═════╝  ╚═════╝ ╚═╝     ╚═╝
+
+'
+end
+
+def dark_cold
+  puts '
+
+      ██╗████████╗███████╗    ██╗   ██╗███████╗██████╗ ██╗   ██╗    ██████╗  █████╗ ██████╗ ██╗  ██╗██╗
+      ██║╚══██╔══╝██╔════╝    ██║   ██║██╔════╝██╔══██╗╚██╗ ██╔╝    ██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝██║
+      ██║   ██║   ███████╗    ██║   ██║█████╗  ██████╔╝ ╚████╔╝     ██║  ██║███████║██████╔╝█████╔╝ ██║
+      ██║   ██║   ╚════██║    ╚██╗ ██╔╝██╔══╝  ██╔══██╗  ╚██╔╝      ██║  ██║██╔══██║██╔══██╗██╔═██╗ ╚═╝
+      ██║   ██║   ███████║     ╚████╔╝ ███████╗██║  ██║   ██║       ██████╔╝██║  ██║██║  ██║██║  ██╗██╗
+      ╚═╝   ╚═╝   ╚══════╝      ╚═══╝  ╚══════╝╚═╝  ╚═╝   ╚═╝       ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝
+
+      ██╗████████╗███████╗    ██╗   ██╗███████╗██████╗ ██╗   ██╗     ██████╗ ██████╗ ██╗     ██████╗ ██╗
+      ██║╚══██╔══╝██╔════╝    ██║   ██║██╔════╝██╔══██╗╚██╗ ██╔╝    ██╔════╝██╔═══██╗██║     ██╔══██╗██║
+      ██║   ██║   ███████╗    ██║   ██║█████╗  ██████╔╝ ╚████╔╝     ██║     ██║   ██║██║     ██║  ██║██║
+      ██║   ██║   ╚════██║    ╚██╗ ██╔╝██╔══╝  ██╔══██╗  ╚██╔╝      ██║     ██║   ██║██║     ██║  ██║╚═╝
+      ██║   ██║   ███████║     ╚████╔╝ ███████╗██║  ██║   ██║       ╚██████╗╚██████╔╝███████╗██████╔╝██╗
+      ╚═╝   ╚═╝   ╚══════╝      ╚═══╝  ╚══════╝╚═╝  ╚═╝   ╚═╝        ╚═════╝ ╚═════╝ ╚══════╝╚═════╝ ╚═╝
+
+  '
+end
+
+def robot
+  puts "
+                                        ,--.    ,--.
+                                       ((O ))--((O ))
+                                     ,'_`--'____`--'_`.
+                                    _:  ____________  :_
+                                   | | ||::::::::::|| | |
+                                   | | ||::::::::::|| | |
+                                   | | ||::::::::::|| | |
+                                   |_| |/__________ | |_|
+                                     |________________|
+                                  __..-'            `-..__
+                               .-| : .----------------. : |-.
+                             ,V || | | ______________/| | || /.
+                           :`-._ V.| || '--''--''--' || |,:/_.-':
+                           |    :  | || .----------. || |  :    |
+                           |    |  | || '----SSt---' || |  |    |
+                           |    |  | ||   _   _   _  || |  |    |
+                           :,--.;  | ||  (_) (_) (_) || |  :,--.;
+                           (`-'|)  | ||______________|| |  (|`-')
+                            `--'   | |/______________ | |   `--'
+                                   |____________________|
+                                    `.________________,'
+                                     (_______)(_______)
+                                     (_______)(_______)
+                                     (_______)(_______)
+                                    |        ||        |
+                                    '--------''--------'
+"
+end
+
+def aliens_go
+  puts "
+                                          _______
+                                      .adOOOOOOOOOba.
+                                     dOOOOOOOOOOOOOOOb
+                                    dOOOOOOOOOOOOOOOOOb
+                                   dOOOOOOOOOOOOOOOOOOOb
+                                  |OOOOOOOOOOOOOOOOOOOOO|
+                                  OP'~'' OOOOOOOOOO '~`YO
+                                  OO     `YOOOOOP'     OO
+                                  OOb      `OOO'      dOO
+                                  YOOo      OOO      oOOP
+                                  `OOOo     OOO     oOOO'
+                                   `OOOb._,dOOOb._,dOOO'
+                                    `OOOOOOOOOOOOOOOOO'
+                                     OOOOOOOOOOOOOOOOO
+                                     YOOOOOOOOOOOOOOOP
+                                     `OOOOOOOOOOOOOOO'
+                                      `OOOOOOOOOOOOO'
+                                       `OOOOOOOOOOO'
+                                         `~OOOOO~'
+  "
+end
+
+def aliens_go_2
+  puts "
+                                          _______
+                                      .adOOOOOOOOOba.
+                                     dOOOOOOOOOOOOOOOb
+                                    dOOOOOOOOOOOOOOOOOb
+                                   dOOOOOOOOOOOOOOOOOOOb
+                                  |OOOOOOOOOOOOOOOOOOOOO|
+                                  OP'~'' OOOOOOOOOO '~`YO
+                                  OO     `YOOOOOP'     OO
+                                  OOb      `OOO'      dOO
+                                  YOOo      OOO      oOOP
+                                  `OOOo     OOO     oOOO'
+                                   `OOOb._,dOOOb._,dOOO'
+                                    `OOOOOOOOOOOOOOOOO'
+                                     OOOOOOO    OOOOOO
+                                     YOOOOOO    OOOOOP
+                                     `OOOOOO    OOOOO'
+                                      `OOOOO    OOOO'
+                                       `OOOOO  OOOO'
+                                         `~OOOOO~'
+  "
+end
+
+def screen_flow (time)
+
+puts '
+
+                         /\               /\              /\               /\
+                    ____/  \____     ____/  \____    ____/  \____     ____/  \____
+                    \          /     \          /    \          /     \          /
+                     >        <       >        <      >        <       >        <
+                    /___    ___\     /___    ___\    /___    ___\     /___    ___\
+                        \  /             \  /            \  /             \  /
+                         \/               \/              \/               \/
+
+        __________________________________________________________________________________________
+        ____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_
+        ___/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\__
+        __/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\___
+        _/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\____
+        /~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____
+        __________________________________________________________________________________________
+
+                         /\               /\              /\               /\
+                    ____/  \____     ____/  \____    ____/  \____     ____/  \____
+                    \          /     \          /    \          /     \          /
+                     >        <       >        <      >        <       >        <
+                    /___    ___\     /___    ___\    /___    ___\     /___    ___\
+                        \  /             \  /            \  /             \  /
+                         \/               \/              \/               \/
+
+'
+
+sleep(time)
+
+puts '
+        __________________________________________________________________________________________
+        ____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_
+        ___/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\__
+        __/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\___
+        _/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\____
+        /~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____
+        __________________________________________________________________________________________
+
+                         /\               /\              /\               /\
+                    ____/  \____     ____/  \____    ____/  \____     ____/  \____
+                    \          /     \          /    \          /     \          /
+                     >        <       >        <      >        <       >        <
+                    /___    ___\     /___    ___\    /___    ___\     /___    ___\
+                        \  /             \  /            \  /             \  /
+                         \/               \/              \/               \/
+
+'
+
+sleep(time)
+
+puts '
+        __________________________________________________________________________________________
+        ____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_
+        ___/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\__
+        __/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\___
+        _/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\____
+        /~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____
+        __________________________________________________________________________________________
+
+                         /\               /\              /\               /\
+                    ____/  \____     ____/  \____    ____/  \____     ____/  \____
+                    \          /     \          /    \          /     \          /
+                     >        <       >        <      >        <       >        <
+                    /___    ___\     /___    ___\    /___    ___\     /___    ___\
+                        \  /             \  /            \  /             \  /
+                         \/               \/              \/               \/
+
+'
+
+sleep(time)
+
+puts '
+        __________________________________________________________________________________________
+        ____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_
+        ___/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\__
+        __/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\___
+        _/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\____
+        /~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____
+        __________________________________________________________________________________________
+
+'
+
+sleep(time)
+
+end
+
+## GAME STARTS HERE
+
+# SECTION 1
+
+screen_flow 0.5
+
+game_title
+
+screen_flow 0.5
+
+game_title
+
+puts "Type 'Start'"
+starter = gets.chomp.downcase
+
+while starter != "start"
+  sleep(0.5)
+  puts "Only Serious Travelers Allowed"
+  starter = gets.chomp.downcase
+end
+
+sleep(0.5)
+
+puts '
+                           _        ___ ______  __  _____      ____   ___
+                          | |      /  _]      ||  |/ ___/     /    | /   \
+                          | |     /  [_|      ||_ (   \_     |   __||     |
+                          | |___ |    _]_|  |_|  \|\__  |    |  |  ||  O  |
+                          |     ||   [_  |  |      /  \ |    |  |_ ||     |
+                          |     ||     | |  |      \    |    |     ||     |
+                          |_____||_____| |__|       \___|    |___,_| \___/
+
+        __________________________________________________________________________________________
+        ____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_
+        ___/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\__
+        __/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\___
+        _/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\____
+        /~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____/~~\_____
+        __________________________________________________________________________________________
+
+'
+
+sleep (2)
+
+screen_flow 0.3
+screen_flow 0.2
+screen_flow 0.2
+screen_flow 0.1
+screen_flow 0.1
+screen_flow 0.1
+screen_flow 0.05
+screen_flow 0.05
+screen_flow 0.05
+screen_flow 0.05
+screen_flow 0.01
+screen_flow 0.01
+screen_flow 0.01
+screen_flow 0.01
+screen_flow 0.01
+
+#SECTION 2
+
+robot
+sleep(2)
+puts "Hello there, Time Traveler!" #time traveler arrives from a portal in space to meet an advanced AI
+sleep(2)
+puts "It's the year 3,100 AD."
+sleep(2)
+puts "My name is Clip. What might your name be?"
+spacing
+name = gets.chomp
+spacing
+sleep(1)
+puts "Hello #{name.capitalize}!"
+sleep(1)
+puts "It's been a long time since I have talked to another conscious being."
+sleep(2)
+puts "It gets very lonely here even for an AI as advanced as me."
+sleep(2)
+spacing
+puts "Anyhow, time is limited, we must leave immediately before others come through the portal."
+sleep(3)
+puts "Where would you like to go: 10,000 years forward, 10,000 years back, or stay here?"
+spacing
+go_to = gets.chomp #user can type variations of the answers
+
+if go_to.include? "back"
+  go_to = "10,000 years back"
+elsif go_to.include? "forward"
+  go_to = "10,000 years forward"
+elsif go_to.include? "stay"
+  go_to = "stay here"
+else
+  puts "Come on! Only Serious Travelers!"
+  go_to = gets.chomp
+end
+
+########
+
+case go_to
+when "10,000 years forward"
+  boom_title
+  screen_flow(0.5)
+  screen_flow(0.1)
+  sleep(0.1)
+  dark_cold
+  sleep(0.1)
+  screen_flow(0.5)
+  screen_flow(0.1)
+  boom_title
+  spacing
+  sleep(1)
+  puts "You arrive to nothing."
+  spacing
+  sleep(1)
+  puts "Inflationary expansion and entropy have destroyed the universe as we know it!"
+  spacing
+  sleep(2)
+  puts "There is nothing Clip can do for..."
+  spacing
+  sleep(1)
+  puts "You are dead. Try again!"
+  spacing
+when "10,000 years back"
+  boom_title
+  screen_flow(0.5)
+  screen_flow(0.1)
+  sleep(0.1)
+  dark_cold
+  sleep(0.1)
+  screen_flow(0.5)
+  screen_flow(0.1)
+  boom_title
+  sleep(1)
+  spacing
+  sleep(1)
+  pyramid
+  spacing
+  sleep(1)
+  puts "You come flying down on top of a pyramid in the middle of an ancient civilization's ceremony"
+  sleep(3)
+  spacing
+  puts "You are regarded as The Messiah!"
+  spacing
+  sleep(2)
+  puts "You must lead your newly found people to progress and knowledge!"
+  spacing
+  sleep(2)
+  puts "Congrats!"
+  spacing
+when "stay here"
+  puts "."
+  sleep(1)
+  puts ".."
+  sleep(1)
+  puts "...."
+  sleep(1)
+  puts "....."
+  sleep(1)
+  puts "......"
+  sleep(1)
+  puts "......."
+  sleep(1)
+  spacing
+  puts "Aliens from another time are coming out from the portal"
+  spacing
+  sleep(2)
+  puts "They are approaching..."
+  spacing
+  sleep(1)
+  aliens_go_2
+  sleep(1)
+  puts "One of them, looks like the leader, steps forward, reaches his extremity out..."
+  spacing
+  sleep(2)
+  puts "What do you want to do?: shake hands or run away"
+  spacing
+  what_to_do = gets.chomp
+
+  case what_to_do
+  when "shake hand"
+    puts "."
+    sleep(0.5)
+    puts ".."
+    sleep(0.5)
+    puts "...."
+    sleep(0.5)
+    spacing
+    puts "Alien rips your whole arm off! You bleed to death!"
+    spacing
+    sleep(1)
+    puts "Try again!"
+    spacing
+  when "run away"
+    puts "."
+    sleep(0.5)
+    puts ".."
+    sleep(0.5)
+    puts "...."
+    spacing
+    sleep(1)
+    puts "Aliens take this as a threat and start shooting their nuclear fision cannons"
+    spacing
+    sleep(2)
+    puts "You were vaporized. There is nothing Clip can do!"
+    spacing
+    sleep(1)
+    puts "Try again!"
+    spacing
+  else
+    puts "I can't help you if you don't want to help yourself!"
+    spacing
+    sleep(2)
+    puts "Aliens eat you alive!"
+    spacing
+    sleep(2)
+    puts "Try again!"
+    spacing
+  end
+else
+  puts "I don't have time for your jokes!"
+  spacing
+  sleep(1)
+  puts "I'm a super advanced AI, but I don't have much of a sense of humor!"
+  spacing
+  sleep(2)
+  puts "Clip sends you back into the portal where you came from!"
+  spacing
+  sleep(2)
+  puts "Try again!"
+  spacing
+end
